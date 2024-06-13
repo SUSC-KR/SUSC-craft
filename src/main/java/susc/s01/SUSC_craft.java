3b3700c6d445383550c2407f2b084b2e9cae803e
JAXPLE <jaxple@gmail.com>
2024-06-14 00:24:03 +0900

    Start Project

---
+++ src/main/java/susc/s01/SUSC_craft.java
@@ -0,0 +1,17 @@
+package susc.s01;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SUSC_craft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
