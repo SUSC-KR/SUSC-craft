a6a9f05e59bb980666981be7fad299bcd35ddfbc
JAXPLE <jaxple@gmail.com>
2024-06-16 22:19:37 +0900

    Add JoinLog Json File && Config File

--- src/main/java/susc/s01/IOHandler/FilePathMap.java
+++ src/main/java/susc/s01/IOHandler/FilePathMap.java
@@ -5,7 +5,7 @@ import susc.s01.SUSC_craft;
 import java.io.File;
 
 public enum FilePathMap {
-    JOIN_LOG(new File(SUSC_craft.getPlugin(SUSC_craft.class).getDataFolder(), "Join.json"));
+    JOIN_LOG(new File(SUSC_craft.getPlugin(SUSC_craft.class).getDataFolder(), "JoinLog.json"));
 
     private final File filePathInstance;
 
