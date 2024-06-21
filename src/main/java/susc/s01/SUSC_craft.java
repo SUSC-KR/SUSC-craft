fb31ea6b86d7518657ccdcb89b4a036263cb3cd8
JwonLee <114869036+JAXPLE@users.noreply.github.com>
2024-06-21 23:16:33 +0900

    Change Class Name & Change Dir

--- src/main/java/susc/s01/SUSC_craft.java
+++ src/main/java/susc/s01/SUSC_craft.java
@@ -3,10 +3,9 @@ package susc.s01;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import susc.s01.Data.IOHandler;
-import susc.s01.Data.Log.JoinLog.PlayerLog;
 import susc.s01.Data.Log.JoinLog.PlayerLogHandler;
 import susc.s01.Event.EventRegister.EventRegister;
-import susc.s01.IOHandler.FilePathMap;
+import susc.s01.IOHandler.FileMap;
 
 public final class SUSC_craft extends JavaPlugin {
     @Override
@@ -35,21 +34,21 @@ public final class SUSC_craft extends JavaPlugin {
 
     private void dataFileLoad() {
         saveDefaultConfig();
-        saveResource(FilePathMap.JOIN_LOG.getFileName(), false);
+        saveResource(FileMap.JOIN_LOG.getFileName(), false);
         Bukkit.getLogger().info(SUSC_craft.class.getName() + "Save Default DataFile");
     }
 
     private void importData() {
-        PlayerLogHandler.getInstance().updateAllUserData(new IOHandler<PlayerLog>().importData(
-                FilePathMap.JOIN_LOG,
+        PlayerLogHandler.getInstance().updateAllUserData(new IOHandler().importData(
+                FileMap.JOIN_LOG,
                 SUSC_craft.class.getName()
                 )
         );
     }
 
     private void exportData() {
-        new IOHandler<PlayerLog>().exportData(
-                FilePathMap.JOIN_LOG,
+        new IOHandler().exportData(
+                FileMap.JOIN_LOG,
                 SUSC_craft.class.getName(),
                 PlayerLogHandler.getInstance().getAllUserTable()
         );
